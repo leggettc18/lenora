fdcde6c7a57a9b038aa44a2ac04cb4f3a42d7803
Christopher Leggett <chris@leggett.dev>
2021-07-28 15:55:15 -0400

    Uses prose for navbar as well.

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -9,6 +9,11 @@ module.exports = {
           fontFamily: `${theme('fontFamily.sans')}`,
           css: {
             color: theme('colors.gray.700'),
+            nav: {
+              a: {
+                color: theme('colors.gray.400'),
+              }
+            },
             h1: {
               color: theme('colors.gray.700'),
             },
