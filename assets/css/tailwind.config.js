d2fac67e1b6434dde2b87b7d60cf99aa3ef40e55
Christopher Leggett <chris@leggett.dev>
2021-07-28 14:01:05 -0400

    Configures typography plugin.

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,6 +1,24 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        'sans': ['Roboto']
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          fontFamily: `${theme('fontFamily.sans')}`,
+          css: {
+            color: theme('colors.gray.700'),
+            h1: {
+              color: theme('colors.gray.700'),
+            },
+            a: {
+              color: "#b794f4",
+            },
+          },
+        },
+      }),
+    },
   },
   variants: {},
   plugins: [
