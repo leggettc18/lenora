0c7f41e31799d407314a45fa67d30461810c36ef
Christopher Leggett <chris@leggett.dev>
2022-03-15 01:53:33 +0000

    Change theme to allow for me.webp with me.jpg fallback

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,40 +1,40 @@
 module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        'sans': ['Roboto']
-      },
-      typography: (theme) => ({
-        DEFAULT: {
-          fontFamily: `${theme('fontFamily.sans')}`,
-          css: {
-            color: theme('colors.gray.700'),
-            nav: {
-              a: {
-                color: theme('colors.gray.400'),
-                '&:hover': {
-                  color: theme('colors.purple.400'),
-                }
-              }
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Roboto']
             },
-            h1: {
-              color: theme('colors.gray.700'),
-            },
-            a: {
-              color: "#b794f4",
-              'text-decoration': "none",
-              '&:hover': {
-                color: "#000000",
-              }
-            },
-          },
+            typography: (theme) => ({
+                DEFAULT: {
+                    fontFamily: `${theme('fontFamily.sans')}`,
+                    css: {
+                        color: theme('colors.gray.700'),
+                        nav: {
+                            a: {
+                                color: theme('colors.gray.400'),
+                                '&:hover': {
+                                    color: theme('colors.purple.400'),
+                                }
+                            }
+                        },
+                        h1: {
+                            color: theme('colors.gray.700'),
+                        },
+                        a: {
+                            color: theme('colors.purple.700'),
+                            'text-decoration': "none",
+                            '&:hover': {
+                                color: "#000000",
+                            }
+                        },
+                    },
+                },
+            }),
         },
-      }),
     },
-  },
-  variants: {},
-  important: "html",
-  plugins: [
-    require('@tailwindcss/typography'),
-  ]
+    variants: {},
+    important: "html",
+    plugins: [
+        require('@tailwindcss/typography'),
+    ]
 }
